5852d9b158cd57c86968955641d66ec13afba89f
flokleiser <132885161+flokleiser@users.noreply.github.com>
2023-11-15 11:13:11 +0100

    working version mi

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,9 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { Assistant } from 'next/font/google'  
 
 const inter = Inter({ subsets: ['latin'] })
+const assistant = Assistant({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Flo Portfolio',
@@ -11,7 +13,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      {/* <body className={inter.className}>{children}</body> */}
+      <body className={assistant.className}>{children}</body>
     </html>
   )
 }
