83fe720c8b6938ac92219b0dc804cfbfd7450040
flokleiser <132885161+flokleiser@users.noreply.github.com>
2023-11-29 15:11:37 +0100

    added some pictures

--- app/projects/creative-coding/page.js
+++ app/projects/creative-coding/page.js
@@ -4,12 +4,19 @@ import { useState, useEffect } from 'react';
 import styles from 'app/page.module.css'
 import { DiGit, DiGithubBadge } from "react-icons/di";
 import Link from 'next/link';
+import Image from 'next/image'
 
+
+const data = [
+  { image : '/images/creative-coding/creativecoding1.png'},
+  { image : '/images/creative-coding/creativecoding2.png'},
+  { image : '/images/creative-coding/creativecoding3.png'},
+];
 
 export default function creativeCoding() {
   const [iframeHeight, setIframeHeight] = useState(0);
   const [iframeWidth, setIframeWidth] = useState(0);
- 
+
 
   useEffect(() => {
     function handleResize() {
@@ -34,7 +41,9 @@ export default function creativeCoding() {
     return () => {
       window.removeEventListener('resize', handleResize);
     };
-  }, []);
+  }, 
+  
+  []);
   useEffect(() => {
     document.title = 'Creative Coding';
   }, []);
@@ -117,6 +126,36 @@ export default function creativeCoding() {
             <p> Examples: </p>
             </div>
             
+            <div className="">
+              <div className="p-3 container mx-auto">
+                <div className="py-2">
+                </div>
+                  <div className="md:flex md:gap-2 md:grid-cols-2 lg:grid-cols-3 mb-12">
+                    {data.map((x) => {
+                      return (
+                        <>
+                 
+                  <article
+                    className="p-3 mb-6  transition duration-300 group transform hover:-translate-y-2 hover:shadow-2xl rounded-2xl cursor-pointer"
+                  >
+                    <div className="relative mb-4 rounded-2xl">
+                      <Image
+                        width={400}
+                        height={400}
+                        className="max-h-80 rounded-2xl w-full object-cover transition-transform duration-300 transform group-hover:scale-105"
+                        src={x.image}
+                        alt=""
+                      />
+                    </div>
+                  </article>
+                  
+                  </>
+                    );
+                  })}
+                </div>
+              </div>
+            </div>
+
             <div style={{ padding: '2rem' }}> </div>
             {/* <code>
               test.js
