2b4c1f4e0c7d3fd9470bb96b867c0cb72b9cae3b
flokleiser <132885161+flokleiser@users.noreply.github.com>
2023-12-01 15:24:06 +0100

    did i fuck up the reduce to the max

--- app/projects/illustrator/page.js
+++ app/projects/illustrator/page.js
@@ -3,8 +3,76 @@ import React, {useState, useEffect} from 'react';
 import styles from 'app/page.module.css'
 import Image from 'next/image'
 import Link from 'next/link'
+import { PiXCircle } from "react-icons/pi"; 
+
+const data = [
+  { image: '/images/illustrator/howtont/HOW-TONT final version_Page_2.jpg', id: 1},
+  { image: '/images/illustrator/howtont/HOW-TONT final version_Page_3.jpg', id: 2},
+  { image: '/images/illustrator/howtont/HOW-TONT final version_Page_4.jpg', id: 3},
+  { image: '/images/illustrator/howtont/HOW-TONT final version_Page_5.jpg', id: 4},
+  { image: '/images/illustrator/howtont/HOW-TONT final version_Page_6.jpg', id: 5},
+];
+
+const data2 = [
+  { image: '/images/illustrator/reduce to the max/Reduce To The Max_Page_2.png', id: 7},
+  { image: '/images/illustrator/reduce to the max/Reduce To The Max_Page_3.png', id: 8},
+  { image: '/images/illustrator/reduce to the max/Reduce To The Max_Page_4.png', id: 9},
+  { image: '/images/illustrator/reduce to the max/Reduce To The Max_Page_5.png', id: 10},
+  { image: '/images/illustrator/reduce to the max/Reduce To The Max_Page_6.png', id: 11},
+];
 
 export default function illustrator() {
+
+
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const handleImageClick = (imageSrc) => {
+    setSelectedImage(imageSrc);
+  };
+
+const handleResetClick = () => {
+    setSelectedImage(null);
+};
+
+useEffect(() => {
+  const handleOutsideClick = (event) => {
+    const imageElement = document.querySelector("#overlay img");
+
+    if (imageElement) {
+       const imageRect = imageElement.getBoundingClientRect();
+
+    if (
+      event.clientX < imageRect.left ||
+      event.clientX > imageRect.right ||
+      event.clientY < imageRect.top ||
+      event.clientY > imageRect.bottom
+      ) {
+        handleResetClick();
+      }
+    }
+  };
+
+  document.addEventListener("mouseup", handleOutsideClick);
+
+  return () => {
+    document.removeEventListener("mouseup", handleOutsideClick);
+  };
+}, []);
+
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+    if (event.key === 'Escape') {
+        handleResetClick();
+    }
+  };
+
+  
+  window.addEventListener('keydown', handleKeyDown);
+
+  return () => {
+    window.removeEventListener('keydown', handleKeyDown);
+  };
+}, []);
 
   useEffect(() => {
     document.title = 'Illustrator';
@@ -59,24 +127,82 @@ export default function illustrator() {
             How To'nt    
             </p>
             </div>
-            <Image src="/images/illustrator/howton't/HOW-TON'T final version_Page_1.jpg"width={800} height={800}/> 
-            <Image src="/images/illustrator/howton't/HOW-TON'T final version_Page_2.jpg"width={800} height={800}/> 
-            <Image src="/images/illustrator/howton't/HOW-TON'T final version_Page_3.jpg"width={800} height={800}/> 
-            <Image src="/images/illustrator/howton't/HOW-TON'T final version_Page_4.jpg"width={800} height={800}/> 
-            <Image src="/images/illustrator/howton't/HOW-TON'T final version_Page_5.jpg"width={800} height={800}/> 
-            <Image src="/images/illustrator/howton't/HOW-TON'T final version_Page_6.jpg"width={800} height={800}/> 
-            
+           
+            <div className="">
+            {selectedImage && (
+        <div id="overlay" 
+        className="fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center focus:outline-none bg-black bg-opacity-75 transition-transform duration-300 z-50">
+          <img
+            src={selectedImage}
+            alt=""
+            className="max-w-4/5 max-h-4/5"
+            style={{ maxHeight: '80vh', zIndex: 9990 }}
+          />
+           <button
+            className="absolute top-5 right-5 bg-white text-black shadow-lg bg-opacity-50 px-2 py-1 rounded"
+            onClick={handleResetClick}
+            style={{ zIndex: 9999 }}
+          >
+            <PiXCircle style={{ fontSize: '2rem' }} />
+          </button>
+        </div>
+      )}
+              <div className="p-3 container mx-auto">
+                <div className="py-2"></div>
+                  <div className="md:flex md:gap-2 md:grid-cols-2 lg:grid-cols-3 mb-12">
+                    {data.map((x) =>  (
+                  <article
+                    className="p-3 mb-6  transition duration-300 group transform hover:-translate-y-2 hover:shadow-2xl rounded-2xl cursor-pointer"
+                  >
+                    <div className="relative mb-4 rounded-2xl">
+                      <Image
+                        width={400}
+                        height={400}
+                        className={`max-h-80 rounded-2xl w-full object-cover transition-transform duration-300 transform group-hover:scale-105 ${
+                          selectedImage ? 'z-0' : ''
+                        }`}
+                        src={x.image}
+                        alt=""
+                        onClick={() => handleImageClick(x.image)}
+                      />
+                    </div>
+                  </article>
+                    ))}
+                  </div>
+                </div>
+              </div>
+              
             <div className={styles.subtitle}>
             <p>
             Reduce To The Max 
             </p>
             </div>
-            <Image src="/images/illustrator/reduce to the max/Reduce To The Max_Page_1.png"width={800} height={800}/> 
-            <Image src="/images/illustrator/reduce to the max/Reduce To The Max_Page_2.png"width={800} height={800}/> 
-            <Image src="/images/illustrator/reduce to the max/Reduce To The Max_Page_3.png"width={800} height={800}/> 
-            <Image src="/images/illustrator/reduce to the max/Reduce To The Max_Page_4.png"width={800} height={800}/> 
-            <Image src="/images/illustrator/reduce to the max/Reduce To The Max_Page_5.png"width={800} height={800}/> 
-            <Image src="/images/illustrator/reduce to the max/Reduce To The Max_Page_6.png"width={800} height={800}/> 
+            <div className="">
+              <div className="p-3 container mx-auto">
+                <div className="py-2"></div>
+                  <div className="md:flex md:gap-2 md:grid-cols-2 lg:grid-cols-3 mb-12">
+                    {data2.map((x) =>  (
+                  <article
+                    className="p-3 mb-6  transition duration-300 group transform hover:-translate-y-2 hover:shadow-2xl rounded-2xl cursor-pointer"
+                  >
+                    <div className="relative mb-4 rounded-2xl">
+                      <Image
+                        width={400}
+                        height={400}
+                        className={`max-h-80 rounded-2xl w-full object-cover transition-transform duration-300 transform group-hover:scale-105 ${
+                          selectedImage ? 'z-0' : ''
+                        }`}
+                        src={x.image}
+                        alt=""
+                        onClick={() => handleImageClick(x.image)}
+                      />
+                    </div>
+                  </article>
+                    ))}
+                  </div>
+                </div>
+              </div>
+           
         </main>
     )
 }
