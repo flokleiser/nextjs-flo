c4ed9a0d029b7d8173c6f51e69cb6942d8058284
flokleiser <132885161+flokleiser@users.noreply.github.com>
2024-01-03 12:07:01 +0100

    even morerer finalisation of animaitons

--- app/projects/animations/page.js
+++ app/projects/animations/page.js
@@ -103,10 +103,10 @@ export default function testing() {
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         exit={{ opacity: 0 }}
-        transition={{ duration: 0.1}}
+        transition={{ duration: 0.06}}
         
         style={{
-          backdropFilter: `blur(4px)`,
+          backdropFilter: `blur(6px)`,
         }}>
         </motion.div>
       )}
