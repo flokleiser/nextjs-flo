794a64f0b7ad8442d636215a7784cb37d9b60fc2
flokleiser <132885161+flokleiser@users.noreply.github.com>
2024-01-11 09:22:44 +0100

    changed page

--- app/projects/tests/filesTest/page.js
+++ app/projects/tests/filesTest/page.js
@@ -109,7 +109,7 @@ z-[-1]"
 
 
 
-      <div style={{ padding: "1rem" }} />
+      {/* <div style={{ padding: "1rem" }} />
       <hr className={styles.pageDivider} />
       <div style={{ padding: "1rem" }} />
 
@@ -144,7 +144,7 @@ z-[-1]"
         </p>
       </div>
         </div>
-      </motion.div>
+      </motion.div> */}
 
       <div style={{ padding: "2rem" }} />
     </main>
