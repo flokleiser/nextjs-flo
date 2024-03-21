71be19acfb89f613ca7de832790aa0b1a50cee95
flononymus <flononymus@gmail.com>
2024-03-21 13:39:35 +0100

    updates suspense tests

--- app/layout.js
+++ app/layout.js
@@ -7,8 +7,21 @@ import { Suspense } from 'react';
 import { useState } from 'react';
 import Loading from './loading.js'
 import { NavbarProvider } from './components/Navbar';
+import { AnimatePresence, motion } from 'framer-motion';
 
 const assistant = Assistant({ subsets: ['latin'] })
+
+const pageVariants = {
+  initial: { opacity: 0, y: 50 },
+  animate: { opacity: 1, y: 0 },
+  exit: { opacity: 0, y: -50 },
+};
+
+const pageTransition = {
+  type: 'tween',
+  ease: 'anticipate',
+  duration: 0.5,
+};
 
 export default function RootLayout({ children }) {
 
@@ -17,14 +30,26 @@ export default function RootLayout({ children }) {
       <head />
       <body className={assistant.className}>
       <Navbar />
-      {/* <NavbarProvider> */}
-      <Suspense fallback={<Loading />}>
-        {/* <div className="pt-[60px]"> */}
-        <div className="pt-[50px]" >
-      <main>{children}</main>
-      </div>
-      </Suspense>
-      {/* </NavbarProvider> */}
+
+      <AnimatePresence mode="wait">
+        <motion.div
+         key={children.key}
+         variants={pageVariants}
+         initial="initial"
+         animate="animate"
+         exit="exit"
+         transition={pageTransition}
+       >
+
+        <Suspense fallback={<Loading />}>
+          <div className="pt-[50px]" >
+          <main>{children}</main>
+          </div>
+        </Suspense>
+
+        </motion.div>
+      </AnimatePresence>
+
       </body>
     </html>
   )
