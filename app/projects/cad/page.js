ef3a4b7d055f8b8da923999491cadac50b465b31
flononymus <flononymus@gmail.com>
2024-03-22 13:48:31 +0100

    updates

--- app/projects/cad/page.js
+++ app/projects/cad/page.js
@@ -9,6 +9,7 @@ import { AnimatePresence, motion } from "framer-motion";
 import { Canvas, useFrame, useLoader } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import ImageGallery from "@/app/components/ImageGallery";
 
 
 function Model({modelPath}) {
@@ -57,114 +58,6 @@ export default function cad() {
     console.log(modelPath)
   };
 
-  // if (modelClosed) {
-  //   butterflyModel = "/stl/butterfly_closed.glb";
-  // } else {
-  //   butterflyModel = "/stl/butterfly_open.glb";
-  //   console.log("check");
-  // }
-
-  const [selectedImage, setSelectedImage] = useState(null);
-  const [selectedDescription, setSelectedDescription] = useState(null);
-  const [currentIndex, setCurrentIndex] = useState(0);
-
-  const handleResetClick = () => {
-    setSelectedImage(null);
-    setCurrentIndex(0);
-    document.documentElement.style.overflow = "auto";
-    document.body.style.overflow = "auto";
-  };
-
-  const handleImageClick = (imageSrc) => {
-    const dataArray = [...data2, ...data3, ...data4];
-    const selectedIndex = dataArray.findIndex(
-      (item) => item.image === imageSrc
-    );
-    if (selectedIndex !== -1) {
-      setSelectedImage(imageSrc);
-      setCurrentIndex(dataArray[selectedIndex].id);
-      document.documentElement.style.overflow = "hidden";
-      document.body.style.overflow = "hidden";
-
-      setSelectedDescription(dataArray[selectedIndex].description);
-    }
-  };
-
-  const handlePrevImage = () => {
-    if (selectedImage !== null) {
-      const dataArray = [...data2, ...data3, ...data4];
-      const prevIndex = dataArray.findIndex((item) => item.id === currentIndex);
-      const newIndex = prevIndex === 0 ? dataArray.length - 1 : prevIndex - 1;
-      setCurrentIndex(dataArray[newIndex].id);
-      setSelectedImage(dataArray[newIndex].image);
-
-      setSelectedDescription(dataArray[newIndex].description);
-    }
-  };
-
-  const handleNextImage = () => {
-    if (selectedImage !== null) {
-      const dataArray = [...data2, ...data3, ...data4];
-      const nextIndex = dataArray.findIndex((item) => item.id === currentIndex);
-      const newIndex = nextIndex === dataArray.length - 1 ? 0 : nextIndex + 1;
-      setCurrentIndex(dataArray[newIndex].id);
-      setSelectedImage(dataArray[newIndex].image);
-
-      setSelectedDescription(dataArray[newIndex].description);
-    }
-  };
-
-  useEffect(() => {
-    const handleOutsideClick = (event) => {
-      const imageElement = document.querySelector("#overlay img");
-      const leftButton = document.querySelector("#leftButton");
-      const rightButton = document.querySelector("#rightButton");
-
-      if (imageElement) {
-        const imageRect = imageElement.getBoundingClientRect();
-
-        if (
-          event.clientX < imageRect.left ||
-          event.clientX > imageRect.right ||
-          event.clientY < imageRect.top ||
-          event.clientY > imageRect.bottom
-        ) {
-          if (
-            event.target !== leftButton &&
-            event.target !== rightButton &&
-            !leftButton.contains(event.target) &&
-            !rightButton.contains(event.target)
-          ) {
-            handleResetClick();
-          }
-        }
-      }
-    };
-
-    document.addEventListener("mouseup", handleOutsideClick);
-
-    return () => {
-      document.removeEventListener("mouseup", handleOutsideClick);
-    };
-  }, []);
-
-  useEffect(() => {
-    const handleKeyDown = (event) => {
-      if (event.key === "Escape") {
-        handleResetClick();
-      } else if (event.key === "ArrowLeft") {
-        handlePrevImage();
-      } else if (event.key === "ArrowRight") {
-        handleNextImage();
-      }
-    };
-
-    window.addEventListener("keydown", handleKeyDown);
-
-    return () => {
-      window.removeEventListener("keydown", handleKeyDown);
-    };
-  }, [handleResetClick, handlePrevImage, handleNextImage]);
 
   useEffect(() => {
     document.title = "Projects - CAD";
@@ -230,70 +123,6 @@ export default function cad() {
           </p>
         </div>
 
-        <div className="">
-          <AnimatePresence>
-            {selectedImage && (
-              <motion.div
-                id="overlay"
-                className="fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center focus:outline-none z-50"
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                exit={{ opacity: 0 }}
-                transition={{ duration: 0.5 }}
-                style={{
-                  backdropFilter: `blur(${selectedImage ? "10px" : "0px"})`,
-                  backgroundColor: "rgba(0, 0, 0, 0.75)",
-                }}
-              >
-                <motion.div
-                  className="flex flex-col items-center mt-8"
-                  initial={{ scale: 0.5 }}
-                  animate={{ scale: 1 }}
-                  exit={{ scale: 0.45 }}
-                  transition={{ duration: 0.3 }}
-                >
-                  <motion.img
-                    src={selectedImage}
-                    alt={selectedImage.description}
-                    className="max-w-4/5 max-h-4/5"
-                    style={{
-                      maxHeight: "80vh",
-                      zIndex: 9998,
-                      borderRadius: "24px 24px 0px 0px ",
-                    }}
-                  />
-                </motion.div>
-
-                <button
-                  className="absolute top-16 right-5 bg-white text-black shadow-lg bg-opacity-50 px-2 py-1 rounded"
-                  onClick={handleResetClick}
-                  style={{ zIndex: 9999 }}
-                >
-                  <PiXCircle style={{ fontSize: "2rem" }} />
-                </button>
-                <div className="absolute left-5 bg-white text-black shadow-lg bg-opacity-50 px-2 py-1 rounded">
-                  <button
-                    id="leftButton"
-                    className="text-black px-0 py-4 rounded-r"
-                    onClick={handlePrevImage}
-                    style={{ zIndex: 9999 }}
-                  >
-                    <IoIosArrowBack style={{ fontSize: "2rem" }} />
-                  </button>
-                </div>
-                <div className="absolute right-5 bg-white text-black shadow-lg bg-opacity-50 px-2 py-1 rounded">
-                  <button
-                    id="rightButton"
-                    className=" text-black px-0 py-4 rounded-r"
-                    onClick={handleNextImage}
-                    style={{ zIndex: 9999 }}
-                  >
-                    <IoIosArrowForward style={{ fontSize: "2rem" }} />
-                  </button>
-                </div>
-              </motion.div>
-            )}
-          </AnimatePresence>
 
           <div style={{ padding: "1rem" }} />
 
@@ -317,7 +146,6 @@ export default function cad() {
             </div>
           </div>
 
-               </div>
 
         <div style={{ padding: "2rem" }} />
         {/* <hr className={styles.pageDivider} /> */}
@@ -387,34 +215,7 @@ export default function cad() {
 
         <div styles={{ padding: "2rem" }}> </div>
 
-        <motion.div
-          className=""
-        >
-          <div className="p-3 container mx-auto">
-            <div className="py-2"></div>
-            <div className="md:flex md:gap-2 md:grid-cols-2 lg:grid-cols-3">
-              {data2.map((x) => (
-                <article
-                  key="cad2"
-                  className="p-3 mb-6  transition duration-300 group transform hover:-translate-y-2 hover:shadow-2xl rounded-2xl cursor-pointer"
-                >
-                  <div className="relative  rounded-2xl">
-                    <Image
-                      width={400}
-                      height={400}
-                      className={`max-h-80 rounded-2xl w-full object-cover transition-transform duration-300 transform group-hover:scale-105 ${
-                        selectedImage ? "z-0" : ""
-                      }`}
-                      src={x.image}
-                      alt=""
-                      onClick={() => handleImageClick(x.image)}
-                    />
-                  </div>
-                </article>
-              ))}
-            </div>
-          </div>
-        </motion.div>
+<ImageGallery data={data2} />
 
         <div styles={{ padding: "2rem" }}> </div>
 
@@ -427,34 +228,7 @@ export default function cad() {
 
         <div styles={{ padding: "2rem" }}> </div>
 
-        <motion.div
-          className=""
-        >
-          <div className="p-3 container mx-auto">
-            <div className="py-2"></div>
-            <div className="md:flex md:gap-2 md:grid-cols-2 lg:grid-cols-3 ">
-              {data3.map((x) => (
-                <article
-                  key="cad3"
-                  className="p-3 mb-6  transition duration-300 group transform hover:-translate-y-2 hover:shadow-2xl rounded-2xl cursor-pointer"
-                >
-                  <div className="relative  rounded-2xl">
-                    <Image
-                      width={400}
-                      height={400}
-                      className={`max-h-80 rounded-2xl w-full object-cover transition-transform duration-300 transform group-hover:scale-105 ${
-                        selectedImage ? "z-0" : ""
-                      }`}
-                      src={x.image}
-                      alt=""
-                      onClick={() => handleImageClick(x.image)}
-                    />
-                  </div>
-                </article>
-              ))}
-            </div>
-          </div>
-        </motion.div>
+        <ImageGallery data={data3} />
 
         <div styles={{ padding: "2rem" }}> </div>
 
@@ -468,34 +242,8 @@ export default function cad() {
 
         <div styles={{ padding: "2rem" }}> </div>
 
-        <motion.div
-          className=""
-        >
-          <div className="p-3 container mx-auto">
-            <div className="py-2"></div>
-            <div className="md:flex md:gap-2 md:grid-cols-2 lg:grid-cols-3">
-              {data4.map((x) => (
-                <article
-                  key="cad4"
-                  className="p-3 mb-6  transition duration-300 group transform hover:-translate-y-2 hover:shadow-2xl rounded-2xl cursor-pointer"
-                >
-                  <div className="relative  rounded-2xl">
-                    <Image
-                      width={400}
-                      height={400}
-                      className={`max-h-80 rounded-2xl w-full object-cover transition-transform duration-300 transform group-hover:scale-105 ${
-                        selectedImage ? "z-0" : ""
-                      }`}
-                      src={x.image}
-                      alt=""
-                      onClick={() => handleImageClick(x.image)}
-                    />
-                  </div>
-                </article>
-              ))}
-            </div>
-          </div>
-        </motion.div>
+        <ImageGallery data={data4} />
+
       </div>
 
       <div style={{ padding: "2rem" }}> </div>
