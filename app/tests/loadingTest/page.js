b9bbd83fab5fcd1582956506d7557150e69a5052
Flo <floriankleiser@gmail.com>
2024-04-08 20:56:23 +0200

    updated spinner

--- app/tests/loadingTest/page.js
+++ app/tests/loadingTest/page.js
@@ -1,27 +1,49 @@
-'use client'
-import styles from 'app/page.module.css'
+"use client";
+import styles from "app/page.module.css";
+
+//very meh
 import { Puff } from "react-loader-spinner";
-// import {Spinner} from "@nextui-org/react";
 
+//meh
+import { Grid } from "react-loader-spinner";
 
-export default function LoadingScreen(){
-    return (
+//this one is pretty good
+import { Hourglass } from "react-loader-spinner";
+
+//pretty good aswell
+import { ProgressBar } from "react-loader-spinner";
+
+//true classic
+import {RotatingLines} from "react-loader-spinner"
+
+//kinda cool
+import {Watch} from "react-loader-spinner"
+
+export default function LoadingScreen() {
+  return (
     <main className={styles.main}>
-
-        <div style={{paddingTop:'5rem'}}/>
-        <div className={styles.subtitleHomepage}>
-        Loading
-        </div>
-            <Puff
-  visible={true}
-  height="180"
-  width="180"
-  color="white"
-  ariaLabel="puff-loading"
-  wrapperStyle={{}}
-  wrapperClass=""
-  />
-
+      <div style={{ paddingTop: "5rem" }} />
+      {/* <div className={styles.subtitleHomepage}>Loading</div> */}
+      <div style={{padding:'10rem'}} />
+      {/* <Puff */}
+      {/* <Grid */}
+      {/* <RotatingLines */}
+      {/* <Watch */}
+      {/* <ProgressBar */}
+      <Hourglass
+        visible={true}
+        height="180"
+        width="180"
+        color="#ffffff"
+        ariaLabel="puff-loading"
+        wrapperStyle={{}}
+        wrapperClass=""
+        colors={["#ffffff", "#ffffff"]}
+        barColor="#ffffff"
+        borderColor="#ffffff"
+        strokeColor="#ffffff"
+        strokeWidth="4"
+      />
     </main>
-    )
+  );
 }
