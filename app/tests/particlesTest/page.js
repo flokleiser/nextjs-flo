b3c1b8ae27d907a7c2a711d1c6e99a185236b5cc
flononymus <flononymus@gmail.com>
2024-04-16 11:25:10 +0200

    canvas test

--- app/tests/particlesTest/page.js
+++ app/tests/particlesTest/page.js
@@ -49,11 +49,18 @@ z-[-1]"
       <div style={{ paddingTop: "1rem" }} />
         <h1 className={styles.titleWithoutPadding}>Particles</h1>
 
-        <div style={{ padding: "1rem" }} />
         <div style={{ padding: "0.5rem" }} />
         <hr className={styles.pageDivider} />
         <div style={{ padding: "0.5rem" }} />
 
+        <canvas style={{
+            width:'80vw', 
+            height:'70vh', 
+            // background:'black'
+            }} 
+            id="scene">
+            </canvas>
+	<input id="copy" type="text" value="Hello Codepen ♥" />
 
         <div style={{ padding: "1rem" }} />
       <div style={{ padding: "2rem" }} />
