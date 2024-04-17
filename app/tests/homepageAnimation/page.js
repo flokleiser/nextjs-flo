8fb88b0b00241008a5d772e74a3762c6d99f26f9
flokleiser <132885161+flokleiser@users.noreply.github.com>
2024-04-17 19:33:32 +0200

    fixed homepage

--- app/tests/homepageAnimation/page.js
+++ app/tests/homepageAnimation/page.js
@@ -120,9 +120,9 @@ export default function Home() {
       <AnimatePresence mode="wait">
       {isHoveringPortfolio ? (
         <motion.div className={styles.homepagePortfolio} style={{ zIndex: -700 }}             
-          initial={{ opacity: 0.1 }}
+          initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
-          exit={{ opacity: 0.1 }}
+          exit={{ opacity: 0 }}
           transition={{ duration: 0.3 }}>
             <div className={styles.cardHomepagePortfolio}
              >
