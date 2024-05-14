c1c212cb8e428d0c6fab143d43801cafb2cae0e9
flononymus <flononymus@gmail.com>
2024-05-14 09:55:57 +0200

    i give up on the uv mapping

--- app/tests/canvasTest/page.js
+++ app/tests/canvasTest/page.js
@@ -16,10 +16,13 @@ import { Orbit } from "next/font/google";
 const ModelViewer = dynamic(() => 
   import( "@/app/components/ModelViewer")
 )
-const modelPath = "/stl/SciFi-Animation noclouds.glb"
+// const modelPath = "/stl/SciFi-Animation noclouds.glb"
+const modelPath = "/stl/uv testing.glb"
 
 
-// ANIMATED TEXTURES MAYBE: https://stemkoski.github.io/Three.js/#model-animation
+// ANIMATED TEXTURES MAYBE: 
+// https://stemkoski.github.io/Three.js/#model-animation
+// https://stackoverflow.com/questions/35610406/how-to-modify-uv-coordinates-with-three-js
 
 function Model({modelPath}){
   // const {scene} = useLoader(GLTFLoader, modelPath)
@@ -35,6 +38,11 @@ function Model({modelPath}){
 
   useFrame((state, delta) => mixer.current?.update(delta));
 
+  const texture = new THREE.TextureLoader().load("/stl/billboard v4.png");
+  texture.wrapS = THREE.RepeatWrapping;
+  texture.wrapT = THREE.RepeatWrapping;
+  texture.repeat.set(4,4);
+
   const prim = useRef();
   const light = useRef();
 
@@ -43,7 +51,9 @@ function Model({modelPath}){
   return (
     <>
 
-  <primitive object={gltf.scene} position={[0, -17, 0]} rotation={[0,0.8,0]}/>;
+  {/* <primitive object={gltf.scene} position={[0, -17, 0]} rotation={[0,0.8,0]}/>; */}
+
+  <primitive object={gltf.scene} position={[0, 0, 0]} rotation={[0,0.8,0]}/>;
   {/* <primitive ref={prim} object={scene} position={[0,-17,0]}  /> */}
   {/* <spotLight ref={light} position={[0,0,0]} intensity={20} distance={10} angle={Math.PI / 4} penumbra={0.5} rotation={[0,0.8,0]} /> */}
   <spotLight ref={light} position={[0,0,0]} intensity={20} distance={10} />
