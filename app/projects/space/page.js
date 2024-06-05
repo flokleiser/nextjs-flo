ce78713e39d9ae7d9da36903a86334ca8eceaf6c
flokleiser <132885161+flokleiser@users.noreply.github.com>
2024-06-05 19:25:18 +0200

    updates model

--- app/projects/space/page.js
+++ app/projects/space/page.js
@@ -35,7 +35,10 @@ const data = [
 
 
 // const modelPath = "/stl/space/AnimationTest2.glb"
-const modelPath = "/stl/space/New animation.glb"
+// const modelPath = "/stl/space/New animation.glb"
+// const modelPath = "/stl/space/New animation2.glb"
+// const modelPath = "/stl/space/New animation3.glb"
+const modelPath = "/stl/space/New animation4.glb"
 
 function Model({modelPath}){
   const gltf = useLoader(GLTFLoader, modelPath)
